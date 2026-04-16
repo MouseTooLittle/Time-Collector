4b34b646dd9601c2c55c01790233f98c15d079a0
MouseTooLittle <jason02chiem@gmail.com>
2026-04-16 02:21:44 -0700

    good now?

--- main.cpp
+++ main.cpp
@@ -14,14 +14,47 @@ Time - Progress
 
 */
 
-#include <iostream>
-using namespace std;
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include <string>  // for basic_string, string, allocator
+#include <vector>  // for vector
 
-enum day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-typedef struct Week {
-    int dayOfWeek;
-};
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Dropdown, Horizontal, Vertical
 
-int main(){
-    return 0;
+int main() {
+  using namespace ftxui;
+
+  std::vector<std::string> entries = {
+      "tribute",     "clearance", "ally",        "bend",        "electronics",
+      "module",      "era",       "cultural",    "sniff",       "nationalism",
+      "negotiation", "deliver",   "figure",      "east",        "tribute",
+      "clearance",   "ally",      "bend",        "electronics", "module",
+      "era",         "cultural",  "sniff",       "nationalism", "negotiation",
+      "deliver",     "figure",    "east",        "tribute",     "clearance",
+      "ally",        "bend",      "electronics", "module",      "era",
+      "cultural",    "sniff",     "nationalism", "negotiation", "deliver",
+      "figure",      "east",
+  };
+
+  int selected_1 = 0;
+  int selected_2 = 0;
+  int selected_3 = 0;
+  int selected_4 = 0;
+
+  auto layout = Container::Vertical({
+      Container::Horizontal({
+          Dropdown(&entries, &selected_1),
+          Dropdown(&entries, &selected_2),
+      }),
+      Container::Horizontal({
+          Dropdown(&entries, &selected_3),
+          Dropdown(&entries, &selected_4),
+      }),
+  });
+
+  auto screen = ScreenInteractive::FitComponent();
+  screen.Loop(layout);
 }
