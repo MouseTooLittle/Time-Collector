e5f98b8d517e238fbb8932e43714ba22154c38b5
MouseTooLittle <jason02chiem@gmail.com>
2026-04-16 02:21:31 -0700

    everything should be setup now??

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+Jason Chiem - Time Collector
+ECE 1310 - Final Project
+
+An stopwatch app that logs time spent. That time spent can also be categorized for classes and assignments.
+
+Goals
+- TUI using FTXUI
+- Full Dates Extension
+- Refer to Doc for more
+
+Time - Progress
+04/16/2026 - Start
+
+*/
+
+#include <iostream>
+using namespace std;
+
+enum day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+typedef struct Week {
+    int dayOfWeek;
+};
+
+int main(){
+    return 0;
+}
